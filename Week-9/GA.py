bb10ad298dd4c82cfdbe21db0d582c79e1bdc24b
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2018-10-17 20:35:29 +0530

    Week 9 Solutions Update 3
    - Complete gradient ascent based algorithm for GMM (untested)

---
+++ Week-9/GA.py
@@ -0,0 +1,172 @@
+import numpy as np
+from scipy.stats import multivariate_normal as mvn
+
+class GMM(object):
+    """
+    Class for gradient ascent based algorithm for a Gaussian Mixture Model.
+
+    Arguments:
+        - X : input dataset
+        - k : number of mixtures to consider
+    """
+    def __init__(self, X, k):
+        self.X = X
+        self.k = k
+
+        # Algorithm specific data
+        # Cluster fraction parameter \pi
+        self.pi = np.full((self.k, ), 1 / self.k)
+
+        # Cluster mean vectors
+        # This is assigned randomly
+        self.mu = [np.random.randn(self.X.shape[1]) for _ in range(self.k)]
+
+        # Cluster covariance matrices
+        # The second step is to make the matrices PD
+        self.sigma = [np.random.randn(self.X.shape[1], self.X.shape[1]) for _ in range(self.k)]
+        self.sigma = [np.matmul(s.T, s) + 1e-05 * np.identity(self.X.shape[1]) for s in self.sigma]
+
+    def _get_probability_matrix(self, X, cluster_prob, means, covariances):
+        """
+        Function to return the matrix of size d x m where entry [j,i] represents
+        the probability of datapoint i w.r.t. jth MVN
+
+        Arguments:
+            - X : data
+            - cluster_prob : cluster probability
+            - means : mean vectors
+            - covariances : covariance matrices
+
+        Returns:
+            - d x m matrix as described above
+        """
+        matrix = np.array([cluster_prob[k_] * mvn(means[k_], covariances[k_]).pdf(X) for k_ in range(len(means))]).T
+        return matrix
+
+    def _pi_grad(self, X, cluster_prob, means, covariances):
+        """
+        Function to get gradients of the cluster probability vector w.r.t. the log likelihood over the dataset
+
+        Arguments:
+            - X :data
+            - cluster_prob : cluster probability
+            - means: mean vectors
+            - covariances : covariance matrices
+
+        Returns:
+            - gradient of cluster probability vector
+        """
+        return 1 / cluster_prob
+
+    def _mu_grad(self, X, cluster_prob, means, covariances):
+        """
+        Function to get gradients of the mean vectors w.r.t. the log likelihood over the dataset
+
+        Arguments:
+            - X : data
+            - cluster_prob : cluster probability
+            - means : mean vectors
+            - covariances : covariance matrices
+
+        Returns:
+            - list of vectors consisting of derivatives of individual mean vectors
+        """
+        prob_matrix = self._get_probability_matrix(X, cluster_prob, means, covariances)
+        mu_grads = []
+        sigmas_inv = [np.linalg.inv(covariances[k_]) for k_ in range(len(means))]
+        for k_ in range(len(means)):
+            weights = prob_matrix[:, k_] / prob_matrix[:, k_].sum()
+            weighted_data = -np.average(X, weights=weights, axis=0)
+            g1 = np.dot(sigmas_inv[k_], weighted_data)
+            assert g1.shape == means[k_].shape, "Size mismatch (_mu_grad)"
+            mu_grads.append(g1)
+        return mu_grads
+
+    def _sigma_grad(self, X, cluster_prob, means, covariances):
+        """
+        Function to get gradients of the covariance matrices w.r.t. the log likelihood over the dataset
+
+        Arguments:
+            - X : data
+            - cluster_prob : cluster probability
+            - means : mean vectors
+            - covariances : covariance matrices
+
+        Returns:
+            - list of matrices consisting of derivatives of individual matrices
+        """
+        prob_matrix = self._get_probability_matrix(X, cluster_prob, means, covariances)
+        sigma_grads = []
+        for k_ in range(len(means)):
+            all_outer_prods = np.array([np.outer(X[i] - means[k_], X[i] - means[k_]) for i in range(len(X))])
+            weights = prob_matrix[:, k_] / prob_matrix[:, k_].sum()
+            cov_inv = np.linalg.inv(covariances[k_])
+            g1 = -cov_inv * 0.5
+            g2 = np.average(all_outer_prods, weights=weights, axis=0) * 0.5
+            assert g1.shape == g2.shape, "Size mismatch (_sigma_grad)"
+            sigma_grads.append(g1 + g2)
+        return sigma_grads
+
+    def _get_log_likelihood(self, X, pis, mus, sigmas):
+        all_probs = np.array([mvn(mus[k_], sigmas[k_]).pdf(X) for k_ in range(len(mus))])
+        all_probs = pis.reshape(-1, 1) * all_probs
+        assert all_probs.shape == (len(mus), X.shape[0]), "Size mismatch in _get_log_likelihood (all_probs)"
+        ll = all_probs.sum(axis=0)
+        assert ll.shape == (X.shape[0],), "Size mismatch in _get_log_likelihood (ll)"
+        ll = np.log(ll).sum()
+        return ll
+
+    def _choose_step(self, X, cluster_prob, means, covariances, alpha=1, tau=0.5):
+        """
+        Function to get the best step size based on Wolfe conditions. Temporary steps are taken with
+        adaptive step size to check if the Wolfe condition is met, and returns the update with the highest
+        gain (loss) in the (negative) log likelihood.
+
+        Arguments:
+            - X : data
+            - cluster_prob : cluster probability
+            - means : mean vectors
+            - covariances : covariance matrices
+            - alpha : starting learning rate. Default: 1
+            - tau : multiplicative factor. Default: 0.5
+
+        Returns:
+            - the modified parameters and the log likelihood at that step
+        """
+        initial_ll = self._get_log_likelihood(X, cluster_prob, means, covariances)
+        while 1:
+            covs_grad = self._sigma_grad(X, cluster_prob, means, covariances)
+            means_grad = self._mu_grad(X, cluster_prob, means, covariances)
+            cluster_prob_grad = self._pi_grad(X, cluster_prob, means, covariances)
+            updated_covs = [covariances[k_] + alpha * covs_grad[k_] + 1e-06 * np.identity(X.shape[1])
+                            for k_ in range(len(means))]
+            updated_means = [means[k_] + alpha * means_grad[k_] for k_ in range(len(means))]
+            updated_cluster_prob = cluster_prob + alpha * cluster_prob_grad
+            try:
+                updated_ll = self._get_log_likelihood(X, updated_cluster_prob, updated_means, updated_covs)
+            except ValueError:
+                alpha = alpha * tau
+                continue
+            if updated_ll > initial_ll:
+                return (updated_cluster_prob, updated_means, updated_covs, updated_ll)
+            alpha = alpha * tau
+
+    def fit(self, iterations=100):
+        """
+        Function to fit the data for a given number of iterations
+
+        Arguments:
+            - iterations : Number of iterations to run the algorithm for. Default: 100
+
+        Returns:
+            - List of log-likelihoods
+        """
+        LLs = []
+
+        for i in range(1, iterations + 1):
+            pi, mu, sigma, LL = self._choose_step(self.X, self.pi, self.mu, self.sigma, alpha=20)
+            self.test_grads(self.X, self.pi, self.mu, self.sigma)
+            self.pi, self.mu, self.sigma = pi, mu, sigma
+            print("{} / {}\tLog Likelihood = {}".format(i, iterations, LL))
+            LLs.append(LL)
+        return LL
--- Week-9/main.py
+++ Week-9/main.py
@@ -1,4 +1,5 @@
-from EM import *
+from EM import GMM as EGMM
+from GA import GMM as GGMM
 
 import numpy as np
 
@@ -7,15 +8,19 @@ from matplotlib import pyplot as plt
 
 p = AP()
 p.add_argument('--dataset', type=str, required=True, help='Dataset file in CSV format')
-p.add_argument('--algorithm', type=str, choices=['EM', 'GD'], default='EM', help='Algorithm to use for mixture model')
+p.add_argument('--algorithm', type=str, choices=['EM', 'GA'], default='EM', help='Algorithm to use for mixture model')
 p.add_argument('--k', type=int, default=2, help='Number of clusters')
 p.add_argument('--iters', type=int, default=100, help='Number of iterations')
 p = p.parse_args()
 
 data = np.genfromtxt(p.dataset, delimiter=',')
 if p.algorithm == 'EM':
-    gmm = GMM(X=data, k=p.k)
+    gmm = EGMM(X=data, k=p.k)
     lls = gmm.fit(iterations=p.iters)
 
-    plt.plot(list(range(1, p.iters + 1)), lls, 'b-', marker='o', mfc='k', mec='k', ms=5.0, lw=3.0)
-    plt.show()
+else:
+	gmm = GGMM(X=data, k=p.k)
+	lls = gmm.fit(iterations=p.iters)
+
+plt.plot(list(range(1, p.iters + 1)), lls, 'b-', marker='o', mfc='k', mec='k', ms=5.0, lw=3.0)
+plt.show()
